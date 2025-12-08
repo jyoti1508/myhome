23296f48084bc2f6700ef96447b33bcc461e794e
Jyoti <kumarijyoti12839@gmail.com>
2025-12-09 00:00:39 +0530

    initial commi

--- Server/configs/cloudinary.js
+++ Server/configs/cloudinary.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     // Directly connect using the URI from .env
     await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("✅ MongoDB Connected Successfully");
+    console.log(" MongoDB Connected Successfully");
   } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error.message);
+    console.error(" MongoDB Connection Error:", error.message);
     process.exit(1); // stop server if DB fails
   }
 };
