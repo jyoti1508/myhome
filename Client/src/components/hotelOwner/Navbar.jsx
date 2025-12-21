cb55861f80a55c5e0870d78e7a9c504213328201
Jyoti <kumarijyoti12839@gmail.com>
2025-12-21 23:34:45 +0530

    Initial commit

--- Client/src/components/hotelOwner/Navbar.jsx
+++ Client/src/components/hotelOwner/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
-import { UserButton } from "@clerk/clerk-react";
+// import { UserButton } from "@clerk/clerk-react";
 
 const NavBar = () => {
   return (
